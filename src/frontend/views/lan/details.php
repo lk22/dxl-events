464611d03f2e413c4b5773d3a7a436a70db52c15
Leo Knudsen <lk22>
2022-05-26 21:50:32 +0200

    update details view with event meta fields

--- src/frontend/views/lan/details.php
+++ src/frontend/views/lan/details.php
@@ -1,19 +1,39 @@
 <?php 
-    var_dump($event);
+    var_dump($settings);
 ?>
 
-<div class="event event-details-container lan-event">
+<div class="event lan-event">
     <div class="details-header">
         <div class="inner">
             <div class="heading">
-                <h1><?php echo $event->title; ?></h1>
+                <h1><?php echo strtoupper($event->title); ?></h1>
             </div>
             <div class="heading-meta">
-                <div class="start-date">
+                <div class="start-date meta-field">
                     <p>Start dato: <?php echo date("j F Y", $event->start); ?></p>
                 </div>
-                <div class="end-date">
+                <div class="end-date meta-field">
                     <p>Slut dato: <?php echo date("j F Y", $event->end); ?></p>
+                </div>
+                <div class="start-time meta-field">
+                    <p>
+                        Dørene åbner kl. <?php echo date("H:i", $settings->start_at); ?>
+                    </p>
+                </div>
+                <div class="end-time meta-field">
+                    <p>
+                        Slutter kl. <?php echo date("H:i", $settings->end_at); ?> 
+                    </p>
+                </div>
+                <div class="participation-start meta-field">
+                    <p>
+                        Tilmelding åbner: <?php echo date("j F Y", $settings->participation_opening_date); ?>
+                    </p>
+                </div>
+                <div class="latest-participation-date meta-field">
+                    <p>
+                        Seneste tilmeldings frist: <?php echo date("j F Y", $settings->latest_participation_date); ?>
+                    </p>
                 </div>
             </div>
             <div class="heading-actions">
