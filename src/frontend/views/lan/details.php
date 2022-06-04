8e651882316660f13a304a12f6c5c9055694de3c
Leo Knudsen <lk22>
2022-06-04 21:41:47 +0200

    implementing bootstrap 5 version of details view

--- src/frontend/views/lan/details.php
+++ src/frontend/views/lan/details.php
@@ -1,6 +1,108 @@
 <?php 
-    // var_dump($participants);
+    // var_dump($settings);
 ?>
+
+<div class="container-fluid event lan-event h-100"">
+    <div class="row event-header p-5 align-items-end" style="height: 600px;">
+        <div class="container">
+            <div class="col-5 text-white">
+                <div class="row title mb-5">
+                    <h1 class="text-white display-5 fw-bold"><?php echo strtoupper($event->title); ?></h1>
+                </div>
+
+                <div class="row meta">
+                    <div class="col-12 location">
+                        <p class="lead text-white fw-bold">
+                            <?php echo $settings->event_location; ?>
+                        </p>
+                    </div>
+                    <div class="col-12 start-date">
+                        <p>Start dato: <?php echo date("j F Y", $event->start); ?></p>
+                    </div>
+                    <div class="col-12 end-date">
+                        <p>Slut dato: <?php echo date("j F Y", $event->end); ?></p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-6">
+                        <button class="btn btn-success modal-button" data-bs-toggle="modal" data-bs-target="#lanEventParticipantsModal">Deltagerliste</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container py-5 overflow-hidden">
+        <div class="row gx-4 tournaments">
+            <div class="col-xl-7 p-5 text-white bg-dark rounded rounded-md">
+            <div class="descriptions info-field mb-4">
+                <h2>Beskrivelse</h2>
+                <?php 
+                    echo $event->description;
+                ?>
+            </div>
+    
+            <div class="extra-information info-field mb-4">
+                <h2>Huskeliste</h2>
+                <p>
+                    <?php echo $event->extra_description; ?>
+                </p>
+            </div>
+    
+            <div class="event-food-details info-field">
+                <h4>Mad priser</h4>
+                <p>
+                    Fredag (Aftensmad): <?php echo $settings->breakfast_friday_price; ?>,-
+                </p>
+                <p>
+                    Lørdag:
+                    <ul>
+                        <li>
+                            Morgenmad: <?php echo $settings->breakfast_saturday_price; ?>,-
+                        </li>
+                        <li>
+                            Frokost: <?php echo $settings->lunch_saturday_price; ?>,-
+                        </li>
+                        <li>
+                            Morgenmad: <?php echo $settings->dinner_saturday_price; ?>,-
+                        </li>
+                    </ul>
+                </p>
+                <p>Søndag (Morgenmad): <?php echo $settings->breakfast_sunday_price ?>,-</p>
+            </div>
+            </div>
+            <div class="col-xl-5 p-4 bg-dark rounded rounded-md">
+                <?php 
+                    foreach( $tournaments as $t => $tournament) {
+                        ?>
+                            <div class="col-12 text-white p-4  rounded rounded-2xl tournament">
+                                <div class="tournament__title">
+                                    <?php echo $tournament->title; ?>
+                                </div>
+                                <div class="tournament__meta">
+                                    <div class="tournament__meta--field">
+                                        <p>Antal deltagere: <?php echo $tournament->participants_count; ?></p>
+                                    </div>
+                                    <div class="tournament__meta--field">
+                                        <p>Starter D. <?php echo date("d F, H:i");?></p>
+                                    </div>
+                                    <div class="tournament__meta--field">
+                                        <button
+                                            class="btn btn-light tournament-button" 
+                                            data-bs-toggle="modal"
+                                            data-bs-target="#lanTournamentModal"
+                                            data-event="<?php echo $event->id; ?>"
+                                            data-tournament="<?php echo $tournament->id; ?>"
+                                            >Se mere</button>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php
+                    }
+                ?>
+            </div>
+        </div>
+    </div>
+</div>
 
 <div class="event lan-event">
     <div class="event__header">
@@ -48,7 +150,7 @@
                         <?php
                     }
                 ?>
-                <button class="modal-button" data-modal="#lanEventParticipantsModal">Deltagerliste</button>
+                <button class="modal-button" data-bs-toggle="modal" data-bs-target="#lanEventParticipantsModal">Deltagerliste</button>
                 <button>Turneringer</button>
             </div>
         </div>
@@ -109,12 +211,7 @@
                                             <p>Starter D. <?php echo date("d F, H:i");?></p>
                                         </div>
                                         <div class="tournament__meta--field">
-                                            <button
-                                                class="button-primary modal-button tournament-button" 
-                                                data-modal="#lanTournamentModal"
-                                                data-event="<?php echo $event->id; ?>"
-                                                data-tournament="<?php echo $tournament->id; ?>"
-                                                >Se mere</button>
+
                                         </div>
                                     </div>
                                 </div>
@@ -130,37 +227,74 @@
     </div>
 </div>
 
-<div class="modal modal-sm" id="lanEventParticipantsModal">
-    <div class="modal-inner">
-        <div class="modal-header">
-            <h1>Deltagerliste</h1>
+<div class="modal modal-lg fade fadeInUp modal-sm" id="lanEventParticipantsModal">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+                <div class="container">
+                    <div class="col-lg-8 col-xl-8">
+                        <h1>Deltagerliste</h1>
+                    </div>
+                    <div class="col-4">
+                        <p><?php echo count($participants); ?> <small>Deltagere</small></p>
+                    </div>
+                </div>
+                <!-- <h1>Deltagerliste <?php echo count($participants); ?> <small>Deltagere</small></h1> -->
+            </div>
+            <div class="modal-body">
+                <div class="event-participants-lists">
+                    <?php 
+                        foreach($participants as $participant) {
+                            ?>
+                                <div class="participant my-4">
+                                    <div class="participant-name lead fw-normal">
+                                        <?php echo $participant->name ?? ""; ?>
+                                    </div>
+                                    <div class="participant-email lead fw-normal">
+                                        <?php echo $participant->gamertag ?? ""; ?>
+                                    </div>
+                                </div>
+                            <?php
+                        }
+                    ?>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button class="close-modal button-primary" data-bs-dismiss="modal">Luk</button>
+            </div>
         </div>
-        <div class="modal-body">
-            <div class="event-participants-lists">
+    </div>
+</div>
+
+<div class="modal modal-lg fade fadeInUp" id="lanTournamentModal">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div class="modal-content">
+            <div class="modal-header">
+
+            </div>
+            <div class="modal-body">
+
+            </div>
+            <div class="modal-footer">
+                <button class="close-modal button-primary" data-bs-dismiss="modal">Luk</button>
                 <?php 
-                    foreach($participants as $participant) {
+                    if( $participated ) {
                         ?>
-                            <div class="participant">
-                                <div class="participant-name">
-                                    <?php echo $participant->name ?? ""; ?>
-                                </div>
-                                <div class="participant-email">
-                                    <?php echo $participant->gamertag ?? ""; ?>
-                                </div>
-                            </div>
+                            <button
+                                data-bs-dismiss="modal"
+                                class="participate-tournament-btn button-success"
+                                data-member="<?php echo $member->id; ?>"
+                            >Tilmeld</button>
                         <?php
                     }
                 ?>
             </div>
         </div>
-        <div class="modal-footer">
-            <button class="close-modal button-primary">Luk</button>
-        </div>
     </div>
 </div>
 
 <!-- modal for showing information about a tournament event attached to LAN event -->
-<div class="modal" id="lanTournamentModal">
+<!-- <div class="modal" id="lanTournamentModal">
     <div class="modal-inner">
         <div class="modal-header">
 
@@ -182,4 +316,4 @@
             ?>
         </div>
     </div>
-</div>
+</div> -->
