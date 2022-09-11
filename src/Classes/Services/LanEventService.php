c5d2dea9577bf27f6e098dcbd9f09d8bf3723f54
Leo Knudsen <lk22>
2022-09-11 14:41:54 +0200

    removing unnecesary service code

--- src/Classes/Services/LanEventService.php
+++ src/Classes/Services/LanEventService.php
@@ -88,45 +88,6 @@
 
                 return $configured ? true : false;
             }
-
-            /**
-             * removing event from database
-             *
-             * @param integer $event
-             * @return boolean
-             */
-            public function removeEvent($event): bool 
-            {
-                $this->removeConfiguration($event);
-                $this->removeParticipants($event);
-                $removed = (new Event())->delete((int) $event);
-                return $removed ? true : false;
-            }
-
-            /**
-             * remove LAN event configuration data
-             *
-             * @param integer $identifier
-             * @return boolean
-             */
-            public function removeConfiguration(int $identifier): bool {
-                $settings = new Settings();
-                $removed = $settings->delete($identifier);
-                return $removed ? true : false;
-            }
-
-            /**
-             * remove LAN event participants
-             *
-             * @param integer $identifier
-             * @return boolean
-             */
-            public function removeParticipants($identifier): bool {
-                $participants = new LanParticipant();
-                $identifier = $participants->setPrimaryIdentifier("event_id");
-                $removed = $participants->delete($identifier);
-                return $removed ? true : false;
-            }
         }
     }
 ?>
