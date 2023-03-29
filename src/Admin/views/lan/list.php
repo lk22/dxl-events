52572639e7b0aba1244f1ae3e4aeac4d0f7c2353
Leo Knudsen <lk22>
2023-03-29 16:41:04 +0200

    chaging design to regular table for lan events

--- src/Admin/views/lan/list.php
+++ src/Admin/views/lan/list.php
@@ -9,10 +9,41 @@
         </div>
     </div>
 
-    <div class="content mt-4">
+    <div class="content mt-4 fadeUp">
         <div class="events-list list">
+            <table class="table table-striped widefat">
+                <tbody>
+                    <?php 
+                        foreach( $events as $event ) {
+                            ?>
+                                <tr>
+                                    <td><?php echo $event->title ?></td>
+                                    <td>
+                                        <?php echo ($event->is_draft) ? "Ikke offentliggjort" : "Offentliggjort" ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $event->tournaments_count ?> Turneringer
+                                    </td>
+                                    <td>
+                                        <?php echo $event->participants_count ?> deltagere
+                                    </td>
+                                    <td>
+                                        Start dato: <span><?php echo date("d-m-Y", $event->start) ?></span>
+                                    </td>
+                                    <td>
+                                        Slut dato: <span><?php echo date("d-m-Y", $event->end) ?></span>
+                                    </td>
+                                    <td>
+                                        <a href="<?php echo generate_dxl_subpage_url(["action" => "details", "id" => $event->id]); ?>" class="button-primary">Se begivenhed</a>
+                                    </td>
+                                </tr>
+                            <?php
+                        }
+                    ?>
+                </tbody>
+            </table>
             <div class="row">
-                <?php require dirname(__FILE__) . "/partials/lan-list.php"; ?>
+                <?php //require dirname(__FILE__) . "/partials/lan-list.php"; ?>
             </div>
         </div>
     </div>
