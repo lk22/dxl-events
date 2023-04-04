838e015f8fa6e86d536f1da0ff2008fb8b5ddb14
Leo Knudsen <lk22>
2023-04-04 22:32:04 +0200

    adding count check for events in specific event type list

--- src/frontend/views/partials/training-event-list.php
+++ src/frontend/views/partials/training-event-list.php
@@ -1,54 +1,58 @@
-<div class="row gap-4 mb-4" id="training">
-  <h3 class="fw-bold px-0">Trænings begivenheder</h3>
-  <?php 
-    if ( isset($events["training"]) && count($events["training"]) ) {
-      foreach ( $events["training"] as $event ) {
-        ?>
-          <div class="event-item col-12 col-sm-12 col-md-6 col-lg-3 event-<?php echo $event["type"] ?>" data-event-type="<?php echo $event["type"] ?>">
-            <div class="event-title">
-              <span>
-                <h4><?php echo $event["title"]; ?></h4>
-              </span>
-            </div>
-            <div class="event-meta row">
-              <div class="event-type col-6">
-                <p>
-                  <span>
-                    <strong>Event type: </strong>
-                  </span>
-                  <span>
-                    <?php echo $event["type"]; ?>
-                  </span>
-                </p>
-              </div>
-              <div class="startdate col-6">
-                <p>
-                  <span>
-                    <strong>Start dato: </strong>
-                  </span>
-                  <span>
-                    <?php echo $event["startdate"]; ?>
-                  </span>
-                </p>
-              </div>
-              <div class="participants-count col-6">
-                <p>
-                  <span>
-                    <strong>
-                      Antal deltagere: 
-                    </strong>
-                  </span>
-                  <span>
-                    <?php echo $event["participants_count"]; ?>
-                  </span>
-                </p>
-              </div>
-              <a href="<?php echo $event["link"]; ?>" class="btn btn-outline-success">Se begivenhed</a>
-            </div>
-          </div>
-        <?php
-      }
-    }
+<?php 
+if ( isset($events["training"]) && count($events["training"]) ) {
   ?>
-  <div class="divider"></div>
-</div>
+    <div class="row gap-4 mb-4" id="training">
+      <h3 class="fw-bold px-0">Trænings begivenheder</h3>
+      <?php 
+          foreach ( $events["training"] as $event ) {
+            ?>
+              <div class="event-item col-12 col-sm-12 col-md-6 col-lg-3 event-<?php echo $event["type"] ?>" data-event-type="<?php echo $event["type"] ?>">
+                <div class="event-title">
+                  <span>
+                    <h4><?php echo $event["title"]; ?></h4>
+                  </span>
+                </div>
+                <div class="event-meta row">
+                  <div class="event-type col-6">
+                    <p>
+                      <span>
+                        <strong>Event type: </strong>
+                      </span>
+                      <span>
+                        <?php echo $event["type"]; ?>
+                      </span>
+                    </p>
+                  </div>
+                  <div class="startdate col-6">
+                    <p>
+                      <span>
+                        <strong>Start dato: </strong>
+                      </span>
+                      <span>
+                        <?php echo $event["startdate"]; ?>
+                      </span>
+                    </p>
+                  </div>
+                  <div class="participants-count col-6">
+                    <p>
+                      <span>
+                        <strong>
+                          Antal deltagere: 
+                        </strong>
+                      </span>
+                      <span>
+                        <?php echo $event["participants_count"]; ?>
+                      </span>
+                    </p>
+                  </div>
+                  <a href="<?php echo $event["link"]; ?>" class="btn btn-outline-success">Se begivenhed</a>
+                </div>
+              </div>
+            <?php
+          }
+      ?>
+      <div class="divider"></div>
+    </div>
+  <?php
+}
+?>
