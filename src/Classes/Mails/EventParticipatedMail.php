eee4d5e156744feccf33bee13751541a0761fd7a
Leo Knudsen <lk22>
2023-08-07 19:40:54 +0200

    refactoring EventParticipatedMail template

--- src/Classes/Mails/EventParticipatedMail.php
+++ src/Classes/Mails/EventParticipatedMail.php
@@ -64,8 +64,8 @@
              * @param [type] $participant
              */
             public function __construct(
-                $participant, 
-                $event, 
+                $participant,
+                $event,
                 $workChoresChecked,
                 $workchores,
                 $seatedMembers = [],
@@ -87,7 +87,7 @@
              *
              * @return void
              */
-            public function send()
+            public function send(): void
             {
                 add_filter('wp_mail_content_type', [$this, 'setContentType']);
                 wp_mail($this->receiver, $this->getSubject(), $this->template(), $this->getHeaders(), $this->getAttachments());
@@ -99,71 +99,91 @@
              *
              * @return void
              */
-            protected function template()
+            protected function template() : string
             {
                 $template = "<h2><strong>Kære " . $this->participant->name . "</strong><h2>\n\n";
                 $template .= "<p>Vi har modtaget din tilmelding til " . $this->event->title . ",</p>\n";
-                
                 $template .= "<p>OBS: vær opmærksom på prisen kan variere efter madvalg</p>\n\n";
 
-                if( $this->seatedMembers) {
-                    $template .= "<p>Vi har noteret os du gerne vil sidde sammen med følgende:</p>\n";
-                    $template .= "<ul>";
-                    foreach( $this->seatedMembers as $member ) {
-                        $template .= "<li>" . $member . "</li>";
-                    }
-                    $template .= "</ul>\n";
-                }
+                $template .= $this->getSeatedMembers($this->seatedMembers);
+                $template .= $this->getCompanion($this->companion); 
+                $template .= $this->getWorkChores(
+                    $this->workChoresChecked, 
+                    $this->workchores
+                );
 
-                if ( $this->companion ) {
-                    $template .= "<p>Vi har noteret os du gerne vil have følgende med som ledsager:</p>\n";
-                    $template .= "<ul>";
-                    $template .= "<li>" . $this->companion["name"] . "</li>";
-                    $template .= (!empty($this->companion["mail"])) ? "<li>" . $this->companion["mail"] . "</li>" : "";
-                    $template .= (!empty($this->companion["phone"])) ? "<li>" . $this->companion["phone"] . "</li>" : "";
-                    $template .= "</ul>\n";
-                }
-
-                if ( $this->workChoresChecked ) {
-                    $template .= "<p>Du ønsker og tage følgende arbejdsopgaver</p>\n";
-                    if ( $this->workchores ) {
-                        $template .= "Fredag: \n";
-                        if ( $this->workchores["participant-work-friday-trash"] ) {
-                            $template .= "<p>Tømning af affald</p>\n";
-                        }
-
-                        if ( $this->workchores["participant-work-friday-fireroute"] ) {
-                            $template .= "<p>Gå brandrunde</p>\n";
-                        }
-
-                        $template .= "\n<h4>Lørdag: </h4>\n";
-                        if ( $this->workchores["participant-work-saturday-trash"] ) {
-                            $template .= "<p>Tømning af affald</p>\n";
-                        }
-
-                        if ( $this->workchores["participant-work-saturday-fireroute"] ) {
-                            $template .= "<p>Gå brandrunde</p>\n";
-                        }
-
-                        $template .= "\n<h4>Lørdag</h4>\n";
-                        if ( $this->workchores["participant-work-sunday-trash"] ) {
-                            $template .= "<p>Tømning af affald</p>\n";
-                        }
-    
-                        if ( $this->workchores["participant-work-sunday-fireroute"] ) {
-                            $template .= "<p>Gå brandrunde</p>\n";
-                        }
-                    }
-                }
-
-                if( !empty($this->notice) ) {
+                if( ! empty($this->notice) ) {
                     $template .= "<p>Vi har noteret din bemærkning</p>\n";
                     $template .= "<p>" . $this->notice . "</p>";
                 }
+
                 $template .= "\n\n<p>Vl glæder os til at se dig</p>";
                 return $template;
-            }   
+            }
+
+            /**
+             * render seated members partial
+             *
+             * @param [type] $seatedMembers
+             * @return string
+             */
+            private function getSeatedMembers($seatedMembers): string 
+            {
+                if( $seatedMembers) {
+                    $partial = "<p>Vi har noteret os du gerne vil sidde sammen med følgende:</p>\n";
+                    $partial .= "<ul>";
+                    foreach( $seatedMembers as $member ) {
+                        $partial .= "<li>" . $member . "</li>";
+                    }
+                    $partial .= "</ul>\n";
+                }
+
+                return $partial ?? "";
+            }
+
+            /**
+             * render companion mail template partial
+             *
+             * @param [type] $companion
+             * @return string
+             */
+            private function getCompanion($companion): string 
+            {
+                if ( $this->companion ) {
+                    $partial = "<p>Vi har noteret os du gerne vil have følgende med som ledsager:</p>\n";
+                    $partial .= "<ul>";
+                    $partial .= "<li>" . $this->companion["name"] . "</li>";
+                    $partial .= (!empty($this->companion["mail"])) ? "<li>" . $this->companion["mail"] . "</li>" : "";
+                    $partial .= (!empty($this->companion["phone"])) ? "<li>" . $this->companion["phone"] . "</li>" : "";
+                    $partial .= "</ul>\n";
+                }
+
+                return $partial ?? "";
+            }
+
+            /**
+             * Render workchores information partial
+             *
+             * @param [type] $checked
+             * @param [type] $workchores
+             * @return string
+             */
+            private function getWorkChores($checked, $workchores): string 
+            {
+                if ( $checked && sizeof($workchores) ) {
+                    $partial = "<p>Du ønsker og tage følgende arbejdsopgaver</p>\n\n";
+
+                    if ( sizeof($workchores) ) {
+                        $partial .= "<ul>";
+                        foreach( $workchores["items"] as $chore ) {
+                            $partial .= "<li>- " . $chore["label"] . "</li>";
+                        }
+                        $partial .= "</ul>";
+                    }
+                }
+
+                return $partial ?? "";
+            }
         }
     }
-
 ?>
